0c7cde852c18710340300089810e53e2f98d5824
Emmanuel <emmanuel.londono@protonmail.com>
2023-04-08 03:27:57 -0600

    Back-end seed fix

--- back-end/migrations/30230405155239_item.js
+++ back-end/migrations/30230405155239_item.js
@@ -6,7 +6,7 @@
 exports.up = function (knex) {
     return knex.schema.createTable('item', table => {
         table.increments('id'); // adds an auto incrementing PK column
-        table.integer('user_id').unsigned()
+        table.integer('user_id').unsigned();
         table.foreign('user_id').references('user.id').deferrable('deferred')
         table.string('item_name');
         table.string('description');
